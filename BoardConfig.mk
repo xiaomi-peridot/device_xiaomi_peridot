4f999c2fca449a1f1ce6c5539afe7fb4ec826cb7
AdarshGrewal <adarshgrewal@gmail.com>
2022-04-03 19:18:28 +0530

    peridot: Include MiuiCamera if exists
    Change-Id: Iee675b4924cb2be7ac2d36c7737b06edc9cadfc9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,6 +82,9 @@ DEVICE_MANIFEST_FILE := \
 
 ODM_MANIFEST_FILES := $(DEVICE_PATH)/configs/hidl/manifest_odm.xml
 
+# Inherit from proprietary files for miuicamera
+-include device/xiaomi/peridot-miuicamera/BoardConfig.mk
+
 # Kernel
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 TARGET_NEEDS_DTBOIMAGE := true
